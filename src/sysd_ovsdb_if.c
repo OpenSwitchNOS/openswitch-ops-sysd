4deefeef197d34bd7384aa693a955c40d1bd801a
Raksha Bangera <raksha.madhava-bangera@hpe.com>
2016-02-23 13:23:48 -0800

    add: dev: show version detail back-end implementation
    Change-Id: I867698892c3365e901c2b08f963a4872d5d45cac
    Signed-off-by: Raksha Bangera <raksha.madhava-bangera@hpe.com>
    Depends-on: Ifdc6b87d4786b8a6d977d445ef365f2b08543e05
    Depends-on: I2e175d9b250c1f90e57c34f95b69e430e4e2aeb8

--- src/sysd_ovsdb_if.c
+++ src/sysd_ovsdb_if.c
@@ -44,6 +44,8 @@
 #include "sysd_util.h"
 #include "sysd_ovsdb_if.h"
 
+#define SRC_REPO_MAX_LEN 128
+
 VLOG_DEFINE_THIS_MODULE(ovsdb_if);
 
 /** @ingroup sysd
@@ -382,6 +384,94 @@ sysd_configure_default_vrf(struct ovsdb_idl_txn *txn,
 }/* sysd_configure_default_vrf */
 
 /*
+ * Function to populate source uri and version of each package/daemon
+ * extracted from /var/lib/version_detail file to "Source_Repository"
+ * table in OVSDB.
+ */
+static void
+sysd_add_src_repo_info(struct ovsdb_idl_txn *status_txn)
+{
+    FILE   *ver_detail_fp              = NULL;
+    char   *file_line                  = NULL;
+    char   *p_field                    = NULL;
+    char   name[SRC_REPO_MAX_LEN]      = "";
+    char   src_uri[SRC_REPO_MAX_LEN]   = "";
+    char   version[SRC_REPO_MAX_LEN]   = "";
+    char   git_hash[SRC_REPO_MAX_LEN]  = "";
+    size_t line_len                    = 0;
+    struct ovsrec_source_repository *src_repo_row = NULL;
+
+    /* Open version_detail file containing version information. */
+    ver_detail_fp = fopen(VERSION_DETAIL_FILE_PATH, "r");
+    if (NULL == ver_detail_fp) {
+        VLOG_ERR("File %s was not found", VERSION_DETAIL_FILE_PATH);
+        return;
+    }
+
+    if (NULL == status_txn) {
+        return;
+    }
+
+    /*
+     * Parse version_detail file line-wise to extract package/daemon name,
+     * corresponding version and its source-URI.
+     */
+    while (getline(&file_line, &line_len, ver_detail_fp) != -1) {
+        sscanf(file_line, "%s %s %s %s", name, git_hash, version, src_uri);
+
+        src_repo_row = ovsrec_source_repository_insert(status_txn);
+
+        if (NULL == src_repo_row) {
+            VLOG_ERR("Could not insert a row into DB\n");
+            return;
+        }
+
+        /* Set Source_Repository name value */
+        p_field = strstr(name, "PKG=");
+        if (p_field && ((p_field + strlen("PKG=")) - name < SRC_REPO_MAX_LEN)) {
+            p_field += strlen("PKG=");
+            ovsrec_source_repository_set_name(src_repo_row, p_field);
+        }
+
+        /* Set Source_Repository version value */
+        p_field = strstr(git_hash, "SRCREV=INVALID");
+        if (NULL == p_field) {
+            p_field = strstr(git_hash, "SRCREV=");
+            if (p_field && ((p_field + strlen("SRCREV=")) - git_hash < SRC_REPO_MAX_LEN)) {
+                p_field += strlen("SRCREV=");
+                ovsrec_source_repository_set_version(src_repo_row, p_field);
+            }
+        } else {
+            p_field = strstr(version, "PV=");
+            if (p_field && ((p_field + strlen("PV=")) - version < SRC_REPO_MAX_LEN)) {
+                p_field += strlen("PV=");
+                ovsrec_source_repository_set_version(src_repo_row, p_field);
+            }
+        }
+
+        /* Set Source_Repository src_uri value */
+        p_field = strstr(src_uri, "SRC_URI=");
+        if (p_field && ((p_field + strlen("SRC_URI=")) - src_uri < SRC_REPO_MAX_LEN)) {
+            p_field += strlen("SRC_URI=");
+            ovsrec_source_repository_set_src_uri(src_repo_row, p_field);
+        }
+
+        /* Reset values */
+        name[0]     = '\0';
+        git_hash[0] = '\0';
+        version[0]  = '\0';
+        src_uri[0]  = '\0';
+    }
+
+    /* Free the memory allocated by getline() */
+    if (NULL != file_line) {
+        free(file_line);
+    }
+    fclose(ver_detail_fp);
+
+} /* sysd_add_src_repo_info */
+
+/*
  * Function to update the software info, e.g. software name, switch version,
  * in the OVSDB retrieved from the Release file.
  */
@@ -543,6 +633,9 @@ sysd_initial_configure(struct ovsdb_idl_txn *txn)
      */
     sysd_update_sw_info(sys);
 
+    /* Populate source uri and version of packages/daemon present in image */
+    sysd_add_src_repo_info(txn);
+
 } /* sysd_initial_configure */
 
 static void
@@ -678,6 +771,17 @@ sysd_run(void)
         } else {
             /* Update the software information. */
             sysd_update_sw_info(cfg);
+
+            txn = ovsdb_idl_txn_create(idl);
+
+            /* Populate source uri and version of packages/daemon present in image */
+            sysd_add_src_repo_info(txn);
+
+            txn_status = ovsdb_idl_txn_commit_block(txn);
+            if (txn_status != TXN_SUCCESS) {
+                VLOG_ERR("Failed to commit the transaction. rc = %u", txn_status);
+            }
+            ovsdb_idl_txn_destroy(txn);
 
             if (!hw_init_done_set) {
                 sysd_chk_if_hw_daemons_done();
