872cf1d8bed618e67bfb0dc15ef66329f0a59af1
Raksha Bangera <raksha.madhava-bangera@hpe.com>
2016-03-15 15:49:09 -0700

    chg: dev: show version detail invocation
    Moving the transaction of populating “Package_Info” table which is required
    for `show version detail` and making it a separate transaction.
    
    Change-Id: I4e6a1413082431dfc003db9ec0bca58f67250e9b
    Signed-off-by: Raksha Bangera <raksha.madhava-bangera@hpe.com>

--- src/sysd_ovsdb_if.c
+++ src/sysd_ovsdb_if.c
@@ -649,9 +649,6 @@ sysd_initial_configure(struct ovsdb_idl_txn *txn)
      */
     sysd_update_sw_info(sys);
 
-    /* Populate source url and version of packages/daemon present in image */
-    sysd_add_package_info(txn);
-
 } /* sysd_initial_configure */
 
 static void
@@ -788,20 +785,21 @@ sysd_run(void)
             /* Update the software information. */
             sysd_update_sw_info(cfg);
 
-            txn = ovsdb_idl_txn_create(idl);
-
-            /* Populate source url and version of packages/daemon present in image */
-            sysd_add_package_info(txn);
-
-            txn_status = ovsdb_idl_txn_commit_block(txn);
-            if (txn_status != TXN_SUCCESS) {
-                VLOG_ERR("Failed to commit the transaction. rc = %u", txn_status);
-            }
-            ovsdb_idl_txn_destroy(txn);
-
             if (!hw_init_done_set) {
                 sysd_chk_if_hw_daemons_done();
             }
+        }
+
+        /* Populate source url, type and version of packages present in image*/
+        if (NULL == ovsrec_package_info_first(idl)) {
+            txn = ovsdb_idl_txn_create(idl);
+            VLOG_INFO("Populating Package_Info\n");
+            sysd_add_package_info(txn);
+            txn_status = ovsdb_idl_txn_commit_block(txn);
+            if (txn_status != TXN_SUCCESS) {
+                VLOG_ERR("Commit failed to Package_Info.rc = %u", txn_status);
+            }
+            ovsdb_idl_txn_destroy(txn);
         }
     }
 
